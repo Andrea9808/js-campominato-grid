6528757f2bda695aedd56690ba4498738cca149e
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-11-28 14:15:59 +0100

    feat : layout

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+//selezioniamo elemento contenitore
+const gridElement = document.getElementById("grid");
+//dichiaro bottone
+const buttonPlay = document.getElementById("buttonplay");
+
+
+buttonPlay.addEventListener("click",
+
+    function () {
+
+        for (let i = 1; i <= 100; i++) {
+
+            const newElement = createMyElement("div", "square", i);
+            gridElement.append(newElement);
+
+            newElement.addEventListener("click",
+
+                function () {
+
+                    newElement.classList.add("clicked");
+
+                }
+            )
+        }
+
+    }
+)
+
+
+
+//funzione per riutilizzo
+function createMyElement(tagtype, classname, content) {
+
+    const currentElement = document.createElement(tagtype);
+    currentElement.classList.add(classname);
+
+    // aggiungo il contenuto (numero) all'elemento
+    currentElement.innerHTML = content;
+
+    return currentElement;
+
+}
