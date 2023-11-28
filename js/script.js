3df9ff7e71ae9e345695ef03feb932271351192e
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-11-28 16:20:38 +0100

    feat: difficoltà

--- js/script.js
+++ js/script.js
@@ -3,29 +3,83 @@
 const gridElement = document.getElementById("grid");
 //dichiaro bottone
 const buttonPlay = document.getElementById("buttonplay");
+//dichiaro il select
+const selectDifficolta = document.getElementById("hard");
 
 
 buttonPlay.addEventListener("click",
 
+
     function () {
 
-        for (let i = 1; i <= 100; i++) {
+        //rimuovo gli elementi dopo aver cliccato nuovamente il bottone
+        gridElement.innerHTML = "";
 
-            const newElement = createMyElement("div", "square", i);
-            gridElement.append(newElement);
+        const difficulty = selectDifficolta.value; // Ottengo la difficoltà selezionata
 
-            newElement.addEventListener("click",
+        
+        //creo una condizione, ad ogni condizione una difficoltà diversa
+        if (difficulty === "Difficoltà1") {
+            for (let i = 1; i <= 100; i++) {
 
-                function () {
+                const newElement = createMyElement("div", "square", i);
+                gridElement.append(newElement);
+    
+                newElement.addEventListener("click",
+    
+                    function () {
+    
+                        newElement.classList.add("clicked");
+    
+                        //click del numero
+                        console.log(i);
+    
+                    }
+                )
+            }
+            
+        } else if (difficulty === "Difficoltà2") {
+            for (let i = 1; i <= 81; i++) {
 
-                    newElement.classList.add("clicked");
+                const newElement = createMyElement("div", "square2", i);
+                gridElement.append(newElement);
+    
+                newElement.addEventListener("click",
+    
+                    function () {
+    
+                        newElement.classList.add("clicked");
+    
+                        //click del numero
+                        console.log(i);
+    
+                    }
+                )
+            }
 
-                    //click del numero
-                    console.log(i);
+        } else if (difficulty === "Difficoltà3") {
+            for (let i = 1; i <= 49; i++) {
 
-                }
-            )
+                const newElement = createMyElement("div", "square3", i);
+                gridElement.append(newElement);
+    
+                newElement.addEventListener("click",
+    
+                    function () {
+    
+                        newElement.classList.add("clicked");
+    
+                        //click del numero
+                        console.log(i);
+    
+                    }
+                )
+            }
+        } else {
+            i = 100; 
         }
+
+        
 
     }
 
